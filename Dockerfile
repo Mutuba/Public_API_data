d92714bed1308bbc6d3d32658d1f3f00ea039b39
Mutuba <danielmutubait@gmail.com>
2021-06-08 10:07:40 +0300

    Fix template errors

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,19 @@
 FROM python:3
 
 
+ENV PATH="/scripts:${PATH}"
+
 WORKDIR /usr/src/app
 
 # set environment variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-ADD . /usr/src/app
+# copy entrypoint.sh
+COPY ./entrypoint.sh .
+
+# copy project
+COPY . .
 
 RUN pip install -r requirements.txt
 
@@ -18,4 +24,4 @@ RUN python manage.py collectstatic --noinput
 CMD gunicorn public_api_data_app.wsgi:application --bind 0.0.0.0:$PORT
 
 # run entrypoint.sh
-# ENTRYPOINT ["entrypoint.sh"]
+# ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
--- public_api_data_app/settings.py
+++ public_api_data_app/settings.py
@@ -9,12 +9,18 @@ import dj_database_url
 from decouple import config
 
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "rest_framework",
+    "django_filters",
     "django.contrib.staticfiles",  # required for serving swagger ui's css/js files
     "drf_yasg",
     "apps.dogs",
@@ -65,15 +71,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = '7!@8iekdn*_6e&x8m!mofryeaw775c&%-nc_)z)+p9w5@bw+(n'
@@ -119,6 +126,7 @@ DATABASES = {
         "PORT": 5432
     }
 }
+
 
 prod_db = dj_database_url.config(conn_max_age=500)
 
