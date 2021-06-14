e2b6df21adbad0c9287aa4e5217de5d7ea91dd6c
Mutuba <danielmutubait@gmail.com>
2021-06-14 14:05:58 +0300

    Fix hosting template issues

--- public_api_data_app/settings.py
+++ public_api_data_app/settings.py
@@ -73,11 +73,9 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = './staticfiles/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 
