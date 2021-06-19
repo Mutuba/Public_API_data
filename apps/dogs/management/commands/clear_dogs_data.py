be5ad8e9d3d4bdbbe1000b9f0037c7852ace2bb5
Mutuba <danielmutubait@gmail.com>
2021-06-19 15:08:40 +0300

    Add changes

--- apps/dogs/management/commands/clear_dogs_data.py
+++ apps/dogs/management/commands/clear_dogs_data.py
@@ -2,8 +2,7 @@ from django.core.management.base import BaseCommand
 
 from apps.dogs.dogs_service import clear_dogs_data
 
-
 class Command(BaseCommand):
     def handle(self, *args, **options):
         clear_dogs_data()
-        print("completed")
+        print("Database cleared")
